99c5d0b122a1353f89ca5b134c5752f23f4a61e7
Naman Jha <jhanaman786@gmail.com>
2022-12-21 23:08:11 +0530

    working

---
+++ index.js
@@ -0,0 +1,9 @@
+var marvel = require("marvel-characters");
+// var superHero = document.getElementById("superhero");
+// var superVillain = document.getElementById("supervillain");
+var supervillains = require("supervillains");
+
+var hero = marvel();
+console.log(hero);
+var villain = supervillains.random();
+console.log(villain);
